5bfd3608b1f2efcde84823a1a733641675d9ea6a
linjiuzhou <linjiuzhou@codemao.cn>
2020-10-20 19:11:17 +0800

    feat: basic axios useage of hook

--- src/components/testHook/testHook.tsx
+++ src/components/testHook/testHook.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+// useState来传数据
+// useEffect来模拟生命周期
 function TestHook() {
   const [data, setData] = useState({
     products: [
@@ -14,12 +17,34 @@ function TestHook() {
     ],
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await axios('https://kkk');
+        setData(result.data);
+      } catch (e) {
+        console.error(e);
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <ul>
-      {data.products.map((i) => (
-        <li key={i.productId}>{i.productName}</li>
-      ))}
-    </ul>
+    <div>
+      {isError && <div>出错了...</div>}
+      {isLoading ? <div>is loading</div> : null}
+      <ul>
+        {data.products.map((i) => (
+          <li key={i.productId}>{i.productName}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
