de773d1aa0b628c0a7c963b44a8a126bb7177860
linjiuzhou <linjiuzhou@codemao.cn>
2020-11-06 11:42:39 +0800

    basic use of route, remain lots TODO

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,11 @@ import {
 import Counter from './components/counter/Counter';
 import TestHook from './components/testHook/testHook';
 import Parent from './components/testLifeCircle/Parent';
+
+import { Page1 } from './view/testRouterView/Page1';
+import { Page2 } from './view/testRouterView/Page2';
+import { Child } from './view/testRouterView/Child';
+
 import { TestRef } from './components/testRef/testRef';
 
 // import { routes } from './router/router';
@@ -15,24 +20,28 @@ function App() {
     <BrowserRouter>
       {/* HashRouter里面一定要有一个根节点，不能直接写Route */}
       <div>
+        <Counter />
+        <TestHook />
+        <TestRef />
+        <Parent />
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">page1</Link>
           </li>
           <li>
-            <Link to="/about">about</Link>
+            <Link to="/page2">page2</Link>
           </li>
           <li>
-            <Link to="/topics">topics</Link>
+            <Link to="/child">child</Link>
           </li>
         </ul>
 
         <hr />
 
         {/* exact 精准匹配 */}
-        <Route exact path="/" component={Counter} />
-        <Route path="/about" component={TestRef} />
-        <Route path="/topics" component={TestHook} />
+        <Route exact path="/" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route path="/child" component={Child} />
       </div>
     </BrowserRouter>
   );
--- src/router/router.ts
+++ src/router/router.ts
@@ -7,7 +7,9 @@ import { Child } from '../view/testRouterView/Child';
  * 看了react-route-config的readme，这还不如不用呢
  * 直接用react-router-dom
  * 顶多就是配置一下，不把route和link写一起
+ * 顺带贴一个学习资源：https://reactrouter.com/web/guides/quick-start
  */
+// TODO:路由嵌套、路由传参等多种形式，照着上面网址学吧，超详细
 
 export const routes = [
   // { path: '/', exact: true, render: () => <Redirect to="/page1" /> },
