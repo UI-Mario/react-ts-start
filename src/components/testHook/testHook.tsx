7da810a6222f2c6ff2e21ae2c919a4f5d3b00889
linjiuzhou <linjiuzhou@codemao.cn>
2020-11-12 11:58:47 +0800

    add some hooks code

--- src/components/testHook/testHook.tsx
+++ src/components/testHook/testHook.tsx
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable max-len */
+import React, {
+  useCallback, useEffect, useReducer, useState,
+} from 'react';
 import axios from 'axios';
 
-// useState来传数据
-// useEffect来模拟生命周期
-// useContext跳过
-// useReducer
+// useState来传数据================y
+// useEffect来模拟生命周期==========
+// useContext跳过=================
+// useReducer感觉用的不普及，因为缺乏统一管理啥的吧，代码还是挺简单的======y
 // useCallback
+// useMemo
+// useRef
+// useImperativeHandle
+// useLayoutEffect
+// useDebugValue
 function TestHook() {
   const [data, setData] = useState({
     products: [
@@ -33,6 +41,9 @@ function TestHook() {
     width: window.innerWidth,
   });
 
+  /**
+   * @description 通过依赖项数组来模拟生命周期
+   */
   useEffect(() => {
     // TODO:执行时机？阻塞？
     console.log('this is useEffect');
@@ -48,8 +59,15 @@ function TestHook() {
       setIsLoading(false);
     };
     fetchData();
-  // }, [count]);
+    // }, [count]);
   }, []);
+
+  /**
+   * @description 至今没搞懂这玩意用来干啥
+   */
+  const testUseCallback = useCallback(() => {
+    console.log('count has been changed');
+  }, [count]);
 
   const handleSetdata = () => {
     // Object.assign和扩展运算符，对于对象内的引用类型，都是浅拷贝
@@ -61,18 +79,43 @@ function TestHook() {
   const handleSetSount = () => {
     setCount(0);
   };
+
+  /**
+   * @description 讲道理也是神奇，我都不用创建store啥的，
+   * 不过这样redux插件也看不见，也不太好
+   */
+  interface testUseReducerStateType {
+    testCount: number;
+  }
+  interface testUseReducerActionType {
+    type: string;
+    payload?: object;
+  }
+  const initialState: testUseReducerStateType = { testCount: 0 };
+  const reducer = (testUseReducerState: testUseReducerStateType, action: testUseReducerActionType) => {
+    switch (action.type) {
+      case 'increment':
+        return { testCount: testUseReducerState.testCount + 1 };
+      case 'decrement':
+        return { testCount: testUseReducerState.testCount - 1 };
+      default:
+        throw new Error();
+    }
+  };
+  const [testUseReducer, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
-      <title>useState:</title>
+      <h5>-------------useState:</h5>
       <span>
         {state.name}
         {state.surname}
         {state.width}
       </span>
-      <title>useEffect:</title>
+      <h5>-------------useEffect:</h5>
       {isError && <div>出错了...</div>}
       {isLoading ? <div>is loading</div> : null}
-      <title>another use of useState</title>
+      <h5>-------------another use of useState</h5>
       <ul>
         {data.products.map((i) => (
           <li key={i.productId}>{i.productName}</li>
@@ -82,8 +125,24 @@ function TestHook() {
       <button type="button" onClick={handleSetSount}>
         change count
       </button>
-      <button type="button" onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
-      <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <button type="button" onClick={() => setCount((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
+        +
+      </button>
+      {testUseCallback()}
+      <h5>-------------test of usereducer</h5>
+      <p>
+        Count:
+        {testUseReducer.testCount}
+      </p>
+      <button type="button" onClick={() => dispatch({ type: 'decrement' })}>
+        -
+      </button>
+      <button type="button" onClick={() => dispatch({ type: 'increment' })}>
+        +
+      </button>
     </div>
   );
 }
